84fa8e3e8896c1b3e4e048de458d3d8b316f49de
Christian Nelson <124488484+cdn316@users.noreply.github.com>
2024-04-04 17:48:35 -0400

    Add files via upload

--- Project2/Model.h
+++ Project2/Model.h
@@ -9,15 +9,66 @@
 using namespace std;
 
 // PC Object
+class PC
+{
+    public:
+        PC() = default;
+};
 
 // Register Object
+class Register
+{
+    public:
+        string instrOp;
+        char regWrite;
+
+        Register() = default;
+
+        Register(Instruction instr)
+        {
+            //come back to make sure instructions "j" and "addi" are taken care of
+            instrOp = instr.getInstrOp();
+
+            if(instrOp == "sw" || instrOp == "beq")
+            {
+                regWrite = '0';
+            }
+            else if(instrOp == "R-type" || instrOp == "lw")
+            {
+                regWrite = '1';
+            }
+        }
+};
 
 // Memory Object
+class Memory
+{
+    public:
+        string instrOp;
+        char memWrite;
+
+        Memory() = default;
+
+        Memory(Instruction instr)
+        {
+            //come back to make sure instructions "j" and "addi" are taken care of
+            instrOp = instr.getInstrOp();
+
+            if(instrOp == "R-type" || instrOp == "lw" || instrOp == "beq")
+            {
+                memWrite = '0';
+            }
+            else if(instrOp == "sw")
+            {
+                memWrite = '1';
+            }
+        }
+};
 
 // Instruction Object
 class Instruction {
     public:
-        unsigned int ALUOp;
+        string ALUOp;
         string InstrOp;
         string FunctionField;
         string DesiredAlUAction;
