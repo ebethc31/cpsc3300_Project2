9103409d83b298626a172eab47115eb7d78b8ce6
ebethc31 <ebethc31@gmail.com>
2024-04-04 21:31:33 -0400

    Update Model.h

--- Project2/Model.h
+++ Project2/Model.h
@@ -8,14 +8,40 @@
 #include <string>
 using namespace std;
 
-// PC Object
+// Instruction Class
+class Instruction {
+    public:
+        string fullInstr;
+        string ALUOp;
+        string DesiredAlUAction;
+        string ALUControlInput;
+        string opcode;
+        string functField;
+        enum class InstrOp{loadWord, storeWord, branchEqual, Rtype};
+        enum class InstrOperation{LW, SW, BEQ, ADD, SUB, AND, OR, SLT, ADDI, J};
+
+
+    // The public variables are all the things that are needed to find the ALU control input
+    public:
+        Instruction() = default;
+        Instruction(string instrct){
+            fullInstr = instrct;
+        };
+        
+        string getOpcode();
+        string getFunctField();
+        InstrOp getInstrOp();
+        InstrOperation getInstrOperation();
+};
+
+// PC Class
 class PC
 {
     public:
         PC() = default;
 };
 
-// Register Object
+// Register Class
 class Register
 {
     public:
@@ -26,21 +52,29 @@ class Register
 
         Register(Instruction instr)
         {
+            Instruction::InstrOp instrOpEnum = instr.getInstrOp();
+            
             //come back to make sure instructions "j" and "addi" are taken care of
-            instrOp = instr.getInstrOp();
-
-            if(instrOp == "sw" || instrOp == "beq")
-            {
+            switch (instrOpEnum){
+            case Instruction::InstrOp::storeWord:
                 regWrite = '0';
-            }
-            else if(instrOp == "R-type" || instrOp == "lw")
-            {
+                break;
+            case Instruction::InstrOp::Rtype:
                 regWrite = '1';
-            }
+                break;
+            case Instruction::InstrOp::loadWord:
+                regWrite = '1';
+                break;
+            case Instruction::InstrOp::branchEqual:
+                regWrite = '0';
+                break;
+            default:
+                break;
+            };
         }
 };
 
-// Memory Object
+// Memory Class
 class Memory
 {
     public:
@@ -51,38 +85,28 @@ class Memory
 
         Memory(Instruction instr)
         {
+            Instruction::InstrOp instrOpEnum = instr.getInstrOp();
+
             //come back to make sure instructions "j" and "addi" are taken care of
-            instrOp = instr.getInstrOp();
-
-            if(instrOp == "R-type" || instrOp == "lw" || instrOp == "beq")
-            {
-                memWrite = '0';
-            }
-            else if(instrOp == "sw")
-            {
+            switch (instrOpEnum){
+            case Instruction::InstrOp::storeWord:
                 memWrite = '1';
-            }
-        }
+                break;
+            case Instruction::InstrOp::Rtype:
+                memWrite = '0';
+                break;
+            case Instruction::InstrOp::loadWord:
+                memWrite = '0';
+                break;
+            case Instruction::InstrOp::branchEqual:
+                memWrite = '0';
+                break;
+            default:
+                break;
+            };
+        };
 };
 
-// Instruction Object
-class Instruction {
-    public:
-        string ALUOp;
-        string InstrOp;
-        string FunctionField;
-        string DesiredAlUAction;
-        string ALUControlInput;
-    private:
-        string opcode;
 
-    // The public variables are all the things that are needed to find the ALU control input
-    public:
-        Instruction() = default;
-        Instruction(string opcode): opcode(opcode){
-
-        }
-        
-};
 
 #endif
