30afe88308bc1309f3de0d81f62634dd983cc3f8
ebethc31 <ebethc31@gmail.com>
2024-04-07 20:56:44 -0400

    Update project2.cpp

--- Project2/Project2/project2.cpp
+++ Project2/Project2/project2.cpp
@@ -1,17 +1,152 @@
-#include "View.h"
-#include "Model.h"
-#include "Controller.h"
+//#include "View.h"
+//#include "Model.h"
+//#include "Controller.h"
 #include <iostream>
 #include <stdio.h>
 #include <string>
 #include <iostream>
+#include <map>
 #include <fstream>
 #include <stdlib.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+
+enum exceptable_Opcodes{
+    Rtype = 0b000000,
+    ADDI = 0b001000, 
+    BEQ = 0b000100,
+    LW =  0b100011,
+    SW = 0b101011,
+    J = 0b000010
+    };
+
+enum RtypeFuncFields{
+    ADD = 0b100000,
+    AND = 0b100100,
+    SUB = 0b100010,
+    OR = 0b100101,
+    SLT = 0b101010
+};
+
 int main(int argc, char* argv[])
 {
+    // PC
+    int PC = 0x00000000;
+
+    // Register
+    string regs[2][8] = {
+            {"R0","R1","R2","R3","R4","R5","R6","R7"},
+            {"0","0","0","0","0","0","0","0"}
+        };
+    //Find the opcode
+    string binaryLine = "00000000010000110000100000100010";
+    string opcode = binaryLine.substr(0,6);
+
+    cout << "Opcode: " << opcode << endl;
+
+    string InstrOp;
+
+    int opcodeValue = stoi(opcode, nullptr, 2);
+
+    switch (opcodeValue)
+    {
+    case Rtype:
+        InstrOp = "Rtype";
+        cout << InstrOp << endl;
+        break;
+    case ADDI:
+        InstrOp = "ADDI";
+        cout << InstrOp << endl;
+        break;
+    case BEQ:
+        InstrOp = "BEQ";
+        cout << InstrOp << endl;
+        break;
+    case J:
+        InstrOp = "J";
+        cout << InstrOp << endl;
+        break;
+    default:
+        cerr << "Invalid opcode detected" << endl;
+        break;
+    }
+
+    // Find funcField
+    string funcField;
+    string desiredALU;
+    if (InstrOp == "Rtype"){
+        funcField = binaryLine.substr(26,6);
+        int funcFieldValue = stoi(funcField, nullptr, 2);
+        switch (funcFieldValue)
+        {
+        case ADD:
+            desiredALU = "ADD";
+            break;
+        case AND:
+            desiredALU = "AND";
+            break;
+        case SUB:
+            desiredALU = "SUB";
+            break;
+        case OR:
+            desiredALU = "OR";
+            break;
+        case SLT:
+            desiredALU = "SLT";
+            break;
+        default:
+            break;
+        }
+    }
+    else if (InstrOp == "LW" || InstrOp == "SW"){
+        funcField = "XXXXXX";
+        desiredALU = "ADD";
+    }
+    else if (InstrOp == "BEQ"){
+        funcField = "XXXXXX";
+        desiredALU = "SUB";
+    }
+    /* ADD STUFF FOR J AND ADDI */
+
+    cout<< "function field: " << funcField << endl;
+    cout<< "desired ALU: " << desiredALU << endl;
+
+    /* INSTRUCTION FORMAT */
+    if(InstrOp == "Rtype"){
+        /* rd */
+        string destination = binaryLine.substr(16,5);
+        int destinationValue = stoi(destination, nullptr, 2);
+        cout << "rd: " << destination << endl;
+        /* rs */
+        string  rs = binaryLine.substr(6,5);
+        int rsValue = stoi(rs, nullptr, 2);
+        cout << "rs: " << rs << endl;
+        /* rt */
+        string rt = binaryLine.substr(11,5);
+        int rtValue = stoi(rt, nullptr, 2);
+        cout << "rt: " << rt << endl;
+    }
+    else if(InstrOp == "LW" || InstrOp == "SW" || InstrOp == "BEQ"){
+        /* rs */
+        string rs = binaryLine.substr(6,5);
+        int rsValue = stoi(rs, nullptr, 2);
+        cout << "rs: " << rs << endl;
+        /* rt */
+        string rt = binaryLine.substr(11,5);
+        int rtValue = stoi(rt, nullptr, 2);
+        cout << "rt: " << rt << endl;
+        /* address */
+        string address = binaryLine.substr(16,5);
+        cout << "address: " << address << endl;
+    }
+    /* ADD SOMETHING FOR J AND ADDI */
+
+    /* REGISTER STUFF */
+    
+    
+    /*
     fstream inFile;
     string line;
 
@@ -26,6 +161,6 @@ int main(int argc, char* argv[])
     }
 
     inFile.close();
-
+    */
     return 0;
 }
