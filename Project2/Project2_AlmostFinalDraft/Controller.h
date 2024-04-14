c8593bc38142d360ded905d65b79ff23a1c31489
ebethc31 <ebethc31@gmail.com>
2024-04-13 20:06:51 -0400

    Add files via upload

---
+++ Project2/Project2_AlmostFinalDraft/Controller.h
@@ -0,0 +1,35 @@
+#ifndef CONTROLLER_H_
+#define CONTROLLER_H_
+#include "Model.h"
+#include "View.h"
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+using namespace std;
+
+class Controller{
+    private:
+        Model m;
+        View v;
+    public:
+        Controller() = default;
+        Controller(Model m, View v) : m(m), v(v){
+            this->m = m;
+            this->v = v;
+        }
+        void decode(string);
+        void execute();
+        void updateView();
+        Model incrementPC(Model);
+        void updateView(Model);
+
+    
+};
+
+
+
+#endif
---
+++ Project2/Project2_AlmostFinalDraft/project2.cpp
@@ -0,0 +1,64 @@
+#include "View.h"
+#include "Model.h"
+#include "Controller.h"
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <map>
+#include <fstream>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+enum exceptable_Opcodes{
+    Rtype = 0b000000,
+    ADDI = 0b001000, 
+    BEQ = 0b000100,
+    LW =  0b100011,
+    SW = 0b101011,
+    J = 0b000010
+    };
+
+enum RtypeFuncFields{
+    ADD = 0b100000,
+    AND = 0b100100,
+    SUB = 0b100010,
+    OR = 0b100101,
+    SLT = 0b101010
+};
+*/
+
+int main(int argc, char* argv[])
+{
+    Model m;
+    View v;
+    string binaryLine = "00001000000000010000000000000001";
+
+    Controller c(m,v);
+
+    c.decode(binaryLine);
+    c.execute();
+    //c.updateView();
+    
+    
+    /*
+    fstream inFile;
+    string line;
+
+    inFile.open(argv[1]);
+
+    //Reads line by line through the input file
+    while(getline(inFile, line))
+    {
+        cout << line << '\n';
+
+        Instruction currInst(line);
+    }
+
+    inFile.close();
+    */
+    return 0;
+}
